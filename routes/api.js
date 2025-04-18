f07c7d0ce9aa1e75643a427f69d334f49e5af1a8
822bigcola <822bigcola@gmail.com>
2025-04-18 15:17:17 +0700

    reverse the list news

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ const News = mongoose.model("News", NewsSchema);
 routerAPI.get("/", async (req, res) => {
   try {
     const news = await News.find();
+    news.reverse();
     console.log(news);
     res.status(200).json(news);
   } catch (err) {
