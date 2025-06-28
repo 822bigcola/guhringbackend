984ccb7f79ec5c071264a25d3dc1594d76c663bc
822bigcola <822bigcola@gmail.com>
2025-06-28 15:49:33 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ const PORT = process.env.PORT || 10000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://822bigcola.github.io", // 👈 Thêm domain này
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
